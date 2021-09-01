9a2c0ecacda24f17d6e588edcbfefca3fcfafc13
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2021-09-01 23:50:45 +0100

    initial commit

---
+++ ClassLibrary/Getter.fs
@@ -0,0 +1,7 @@
+﻿namespace ClassLibrary
+
+// F# equivalent of `public delegate FieldType Getter<DeclaringType, FieldType>(in DeclaringType);`
+type Getter<'DeclaringType, 'FieldType> = delegate of inref<'DeclaringType> -> 'FieldType 
+
+type GetterWrapper<'DeclaringType, 'FieldType> (getter : Getter<'DeclaringType, 'FieldType>) =
+    member _.Get (instance : 'DeclaringType) = getter.Invoke &instance
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using ClassLibrary;
+
+GetterWrapper<MyRecord, int[]> wrapper = new((in MyRecord record) => record.Value);
+
+MyRecord record = new MyRecord(new int[] { 42 });
+int[] value = wrapper.Get(record);
+value.GetEnumerator(); // segmentation fault
+
+public record MyRecord(int[] Value);
